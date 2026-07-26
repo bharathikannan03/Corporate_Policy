e788336cc3f5b7ec27dfef45154491556aa899fc
Hem-Akash-97 <hemnathbalakumar19@gmail.com>
2026-07-27 00:36:46 +0530

    updated production url

--- config/runtime.exs
+++ config/runtime.exs
@@ -83,19 +83,19 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+host =
+  System.get_env("PHX_HOST") || "vibe-elixir.up.railway.app"
 
-  config :corporate_policy, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+config :corporate_policy, :dns_cluster_query,
+  System.get_env("DNS_CLUSTER_QUERY")
 
-  config :corporate_policy, CorporatePolicyWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://bandit.hexdocs.pm/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
-    ],
+config :corporate_policy, CorporatePolicyWeb.Endpoint,
+  url: [host: host, port: 443, scheme: "https"],
+  check_origin: ["https://#{host}"],
+  http: [
+    ip: {0, 0, 0, 0},
+    port: String.to_integer(System.get_env("PORT") || "4000")
+  ],
     secret_key_base: secret_key_base
 
   # ## SSL Support

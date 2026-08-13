b6d1f8db68aa12b4ed71f39c8a3dfa48abbe7774
Hem-Akash-97 <hemnathbalakumar19@gmail.com>
2026-08-13 17:47:42 +0530

    updated migration for role visibility id

--- priv/repo/migrations/20260726152014_add_ref_feature_template_field_id_to_md_visibility_role_feature_tmps.exs
+++ priv/repo/migrations/20260726152014_add_ref_feature_template_field_id_to_md_visibility_role_feature_tmps.exs
@@ -1,16 +1,36 @@
 defmodule CorporatePolicy.Repo.Migrations.AddRefFeatureTemplateFieldIdToMdVisibilityRoleFeatureTmps do
   use Ecto.Migration
 
-  def change do
-    alter table(:md_visibility_role_id_feature_tmps) do
-      add :ref_feature_template_field_id,
-          references(
-            :master_policy_feature_template_fields,
-            column: :template_field_id,
-            type: :bigint,
-            on_delete: :delete_all
-          ),
-          null: true
+  def up do
+    has_column =
+      repo().query!(
+        "SELECT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'md_visibility_role_id_feature_tmps' AND column_name = 'ref_feature_template_field_id')"
+      ).rows
+      |> List.first()
+      |> List.first()
+
+    if has_column do
+      alter table(:md_visibility_role_id_feature_tmps) do
+        modify :ref_feature_template_field_id,
+               references(
+                 :master_policy_feature_template_fields,
+                 column: :template_field_id,
+                 type: :bigint,
+                 on_delete: :delete_all
+               ),
+               null: true
+      end
+    else
+      alter table(:md_visibility_role_id_feature_tmps) do
+        add :ref_feature_template_field_id,
+            references(
+              :master_policy_feature_template_fields,
+              column: :template_field_id,
+              type: :bigint,
+              on_delete: :delete_all
+            ),
+            null: true
+      end
     end
 
     create_if_not_exists(
@@ -20,5 +40,19 @@ defmodule CorporatePolicy.Repo.Migrations.AddRefFeatureTemplateFieldIdToMdVisibi
         name: :md_visibility_role_id_feature_tmps_template_field_idx
       )
     )
+  end
+
+  def down do
+    drop_if_exists(
+      index(
+        :md_visibility_role_id_feature_tmps,
+        [:ref_feature_template_field_id],
+        name: :md_visibility_role_id_feature_tmps_template_field_idx
+      )
+    )
+
+    alter table(:md_visibility_role_id_feature_tmps) do
+      remove :ref_feature_template_field_id
+    end
   end
 end

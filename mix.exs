b93dd6d0ae3cc6f0a5d1bfac2d3de252b6507e2f
Hem-Akash-97 <hemnathbalakumar19@gmail.com>
2026-08-21 18:49:46 +0530

    added ecto.seed command to mix.exs

--- mix.exs
+++ mix.exs
@@ -80,7 +80,8 @@ defmodule CorporatePolicy.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
+      "ecto.seed": ["run priv/repo/seeds/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.drop --quiet", "ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
